cb14a8bdb56913aadea26642f75f78d042557dc2
burn-c <chopcsom@gmail.com>
2019-12-05 00:01:35 -0300

    Config: Redux/Store

---
+++ frontend/src/store/modules/auth/reducer.js
@@ -0,0 +1,10 @@
+const INITIAL_STATE = {
+  token: '1515616156161651',
+};
+
+export default function auth(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}

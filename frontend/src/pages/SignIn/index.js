13027c6867898562f4dc6e65d70d90afa9b43d75
burn-c <chopcsom@gmail.com>
2019-12-04 23:18:59 -0300

    Style: SignIn

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
-
 // import { Container } from './styles';
+import logo from '~/assets/logo.png';
 
 export default function SignIn() {
-  return <h1>SignIn</h1>;
+  return (
+    <>
+      <img src={logo} alt="GymPoint" />
+      <form>
+        <input type="email" placeholder="Digite seu e-mail" />
+        <input type="password" placeholder="Digite seu senha" />
+
+        <button type="submit">Acessar Painel</button>
+      </form>
+    </>
+  );
 }
--- frontend/src/pages/_layouts/auth/styles.js
+++ frontend/src/pages/_layouts/auth/styles.js
@@ -1,7 +1,63 @@
 import styled from 'styled-components';
+import {darken} from 'polished';
 
 export const Wrapper = styled.div`
   height: 100%;
   width: 100%;
-  background: #0e0;
+  background: linear-gradient(152deg, rgba(233,68,112,1) 0%, rgba(246,83,200,1) 100%);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  height: 350px;
+  max-width: 320px;
+  width: 100%;
+  background: rgba(255, 255, 255, 0.7);
+  text-align: center;
+  border-radius: 10px;
+  box-shadow: 0px 0px 14px 0px rgba(0,0,0,0.75);
+  padding: 12px;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 20px;
+  }
+
+  input {
+    background: rgba(0, 0, 0, 0.1);
+    border: 0;
+    border-radius: 2px;
+    height: 40px;
+    padding: 0 15px;
+    color: #000;
+    margin: 10px;
+
+    &::placeholder {
+      color: rgba(0, 0, 0, 1);
+    }
+  }
+  button {
+    margin: 5px 0 0;
+    height: 50px;
+    background: radial-gradient(
+      circle,
+      rgba(19, 201, 134, 1) 0%,
+      rgba(0, 255, 169, 1) 150%
+    );
+    color: #fff;
+    font-weight: bold;
+    border: 0;
+    border-radius: 10px;
+    font-size: 20px;
+    margin: 10px;
+    transition: background 1s;
+
+    &:hover {
+      background: ${darken(0.00005, '#13c986' ) };
+    }
+  }
 `;

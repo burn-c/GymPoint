be47f8a81614eb05cc2c456733a5fa30b1bf0547
burn-c <chopcsom@gmail.com>
2019-12-05 21:50:17 -0300

    Feature: Authentication

--- frontend/src/routes/Route.js
+++ frontend/src/routes/Route.js
@@ -5,13 +5,15 @@ import { Route, Redirect } from 'react-router-dom';
 import AuthLayout from '~/pages/_layouts/auth';
 import DefaultLayout from '~/pages/_layouts/default';
 
+import store from '~/store';
+
 export default function RouteWrapper({
   component: Component,
   isPrivate,
   ...rest
 }) {
   // Armazena o estado se o usúario esta logado
-  const signed = false;
+  const { signed } = store.getState().auth;
 
   if (!signed && isPrivate) {
     return <Redirect to="/" />;
--- frontend/src/store/modules/auth/reducer.js
+++ frontend/src/store/modules/auth/reducer.js
@@ -1,9 +1,19 @@
+// Manipular o estado
+import produce from 'immer';
+
 const INITIAL_STATE = {
-  token: '1515616156161651',
+  token: null,
+  signed: false,
+  loading: false,
 };
 
 export default function auth(state = INITIAL_STATE, action) {
   switch (action.type) {
+    case '@auth/SIGN_IN_SUCCESS':
+      return produce(state, draft => {
+        draft.token = action.payload.token;
+        draft.signed = true;
+      });
     default:
       return state;
   }

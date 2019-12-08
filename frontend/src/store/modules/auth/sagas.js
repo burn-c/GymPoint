b149c4221708da75e1d2ebf033bb72098ca2b3f6
burn-c <chopcsom@gmail.com>
2019-12-08 14:52:30 -0300

    Feature: DisplayingToasts

--- frontend/src/store/modules/auth/sagas.js
+++ frontend/src/store/modules/auth/sagas.js
@@ -1,5 +1,7 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
 
+import { toast } from 'react-toastify';
+
 import { signInSuccess, signFailure } from './actions';
 
 import history from '~/services/history';
@@ -17,13 +19,14 @@ export function* signIn({ payload }) {
     const { token, user } = response.data;
 
     if (!user.provider) {
-      console.tron.error('Usuário não é prestador');
+      toast.error('Usuário não é prestador');
     }
 
     yield put(signInSuccess(token, user));
 
     history.push('/students');
   } catch (err) {
+    toast.error('Falha na autenticação, verifique seus dados');
     yield put(signFailure());
   }
 }

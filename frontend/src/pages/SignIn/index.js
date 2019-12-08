52c958819e5d3072b5de3b714d2c262e0cca4b94
burn-c <chopcsom@gmail.com>
2019-12-08 14:44:41 -0300

    Feature: AuthenticationLoading

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
@@ -16,6 +16,7 @@ const schema = Yup.object().shape({
 
 export default function SignIn() {
   const dispatch = useDispatch();
+  const loading = useSelector(state => state.auth.loading);
 
   function handleSubmit({ email, password }) {
     dispatch(signInRequest(email, password));
@@ -28,7 +29,9 @@ export default function SignIn() {
         <Input name="email" type="email" placeholder="Digite seu e-mail" />
         <Input name="password" type="password" placeholder="Digite seu senha" />
 
-        <button type="submit">Acessar Painel</button>
+        <button type="submit">
+          {loading ? 'Carregando...' : 'Acessar Painel'}
+        </button>
       </Form>
     </>
   );

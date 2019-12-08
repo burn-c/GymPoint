7439a4e065514a0eec92dfb5bf48a46e3e1a7871
burn-c <chopcsom@gmail.com>
2019-12-08 15:04:47 -0300

    Feature: AuthenticatedRequests

--- frontend/src/store/modules/auth/sagas.js
+++ frontend/src/store/modules/auth/sagas.js
@@ -22,6 +22,7 @@ export function* signIn({ payload }) {
       toast.error('Usuário não é prestador');
     }
 
+    api.defaults.headers.Authorization = `Bearer ${token}`;
     yield put(signInSuccess(token, user));
 
     history.push('/students');
@@ -31,4 +32,17 @@ export function* signIn({ payload }) {
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+]);

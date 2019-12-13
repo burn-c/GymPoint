2128a63b78aa792f7d6af84e069f910d8afbe507
Carlos Oliveira <chopcsom@gmail.com>
2019-12-13 14:03:31 +0000

    Layout: PlanCreate

---
+++ frontend/src/pages/PlanCreate/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Container } from '../Students/styles';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+export default function PlansCreate() {
+
+
+  return (
+    <Container>
+      <Form>
+        <label>
+          <Input>
+          </Input>
+        </label>
+      </Form>
+    </Container>
+  )
+}
---
+++ frontend/src/pages/PlansCreate/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Container } from '../Students/styles';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+export default function PlanCreate() {
+
+
+  return (
+    <Container>
+      <Form>
+        <label>
+          <Input>
+          </Input>
+        </label>
+      </Form>
+    </Container>
+  )
+}

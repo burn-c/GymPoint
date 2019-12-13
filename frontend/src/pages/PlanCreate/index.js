a88ba1ae9cd7ff04351a0016e0a269c7615dc340
Carlos Oliveira <chopcsom@gmail.com>
2019-12-13 14:11:55 +0000

    Layout: PlanCreate/Text

--- frontend/src/pages/PlanCreate/index.js
+++ frontend/src/pages/PlanCreate/index.js
@@ -3,13 +3,28 @@ import { Container } from '../Students/styles';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
-export default function PlansCreate() {
+export default function PlanCreate() {
 
 
   return (
     <Container>
       <Form>
         <label>
+          TÍTULO DO PLANO
+          <Input name="plan" type="text" placeholder="Digite seu e-mail" />
+        </label>
+        <label>
+          DURAÇÃO (em meses)
+          <Input>
+          </Input>
+        </label>
+        <label>
+          PREÇO MENSAL
+          <Input>
+          </Input>
+        </label>
+          <label>
+          PREÇO TOTAL
           <Input>
           </Input>
         </label>
